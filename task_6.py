ea7a928307edfc6949b79dbad29f38542b97bfa0
Stas <stas.krivosheev.0054@gmail.com>
2023-01-27 21:05:22 +0300

    first commit

---
+++ task_6.py
@@ -0,0 +1,14 @@
+n_1 = int(input("первое число: "))
+n_2 = int(input("второе число: "))
+n_3 = int(input("третье число: "))
+n_4 = int(input("четвертое число: "))
+n_5 = int(input("пятое число: "))
+n_6 = int(input("шестое число: "))
+
+sum_one = n_1+n_2+n_3
+sum_two = n_4+n_5+n_6
+
+if sum_one == sum_two:
+    print("Yes")
+else:
+    print("No")
